c3e09974537afba37e469198f87fd508f81cdb0f
Med-IT-Lab <y4vqwq8ygf@privaterelay.appleid.com>
2026-05-15 05:42:55 +0000

    add work buttons

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const studyTime = document.querySelector("#study-time");
 timeButton.addEventListener("click", function() {
     studyTime.textContent = timeInput.value;
 });
+
+const todoInput = document.querySelector("#input-todo");
+const todoButton = document.querySelector("#save-to-do");
+const todaytodo = document.querySelector("#today-to-do");
+
+todoButton.addEventListener("click", function() {
+    todaytodo.textContent = todoInput.value;
+});
+
+const workInput = document.querySelector("#work-input");
+const workButton = document.querySelector("#save-work");
+const todayWork = document.querySelector("#today-work");
+
+workButton.addEventListener("click", function() {
+    todayWork.textContent = workInput.value;
+});
