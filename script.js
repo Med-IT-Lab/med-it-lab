3d168ddf48b15ee77d3e93d823b840b271ccc64a
Med-IT-Lab <y4vqwq8ygf@privaterelay.appleid.com>
2026-05-15 05:17:15 +0000

    ボタン治し

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const input = document.querySelector("#study-time");
-const button = document.querySelector("#save-button");
+const input = document.querySelector("#time-input");
+const button = document.querySelector("#save-time");
 const studyTime = document.querySelector("#study-time");
 
 button.addEventListener("click", function() {
     studyTime.textContent = input.value;
 });
 
-const input = document.querySelector("#today-work");
-const button = document.querySelector("#save-button");
+const input = document.querySelector("#work-input");
+const button = document.querySelector("#save-work");
 const todayWork = document.querySelector("#today-work");
 
 button.addEventListener("click", function() {
     todayWork.textContent = input.value;
+});
+
+const input = document.querySelector("#input-to-do");
+const button = document.querySelector("#save-to-do");
+const studyTime = document.querySelector("#today-to-do");
+
+button.addEventListener("click", function() {
+    studyTime.textContent = input.value;
 });
