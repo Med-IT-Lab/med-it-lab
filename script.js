65e76638d5a6baf97ec1bb338ab9851b7f2cb3c4
Med-IT-Lab <y4vqwq8ygf@privaterelay.appleid.com>
2026-05-15 05:34:59 +0000

    エラー対処

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
-const timeinput = document.querySelector("#time-input");
-const timebutton = document.querySelector("#save-time");
+const timeInput = document.querySelector("#time-input");
+const timeButton = document.querySelector("#save-time");
 const studyTime = document.querySelector("#study-time");
 
-timebuttonbutton.addEventListener("click", function() {
-    studyTime.textContent = timeinputinput.value;
-});
-
-const workinput = document.querySelector("#work-input");
-const workbutton = document.querySelector("#save-work");
-const todayWork = document.querySelector("#today-work");
-
-button.addEventListener("click", function() {
-    todayWork.textContent = workinput.value;
-});
-
-const todoinput = document.querySelector("#input-to-do");
-const todobutton = document.querySelector("#save-to-do");
-const todo = document.querySelector("#today-to-do");
-
-button.addEventListener("click", function() {
-    studyTime.textContent = todoinput.value;
+timeButton.addEventListener("click", function() {
+    studyTime.textContent = timeInput.value;
 });
